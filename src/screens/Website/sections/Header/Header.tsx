a10356ecdd31ddfa9fc6e719bdad19615b48bb06
mariamoniztome <maria.tome@builtcolab.pt>
2026-01-02 09:44:01 +0000

    feat: update Header component layout for improved structure and clarity

--- src/screens/Website/sections/Header/Header.tsx
+++ src/screens/Website/sections/Header/Header.tsx
@@ -2,9 +2,9 @@ import TicketButton from "../../components/TicketButton";
 
 export const Header = (): JSX.Element => {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 h-[95px]">
-      <div className="mx-4 flex h-full items-center justify-between ">
-        {/* Logo */}
+    <header className="relative z-40 h-[95px]">
+      <div className="mx-4 flex h-full items-center justify-between">
+        {/* Logo (scrolla normalmente) */}
         <div className="flex items-center gap-2">
           <img
             src="/logo-orange.svg"
@@ -12,8 +12,10 @@ export const Header = (): JSX.Element => {
             className="h-10 w-auto sm:h-20"
           />
         </div>
+      </div>
 
-        {/* CTA */}
+      {/* CTA FIXO */}
+      <div className="fixed top-6 right-6 z-50">
         <TicketButton />
       </div>
     </header>
